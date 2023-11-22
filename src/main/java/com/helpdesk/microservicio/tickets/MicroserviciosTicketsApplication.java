245b9d9f4fab123650b19170492df0b3014212ff
Giovanni Roque <giovfranz@gmail.com>
2023-11-21 23:15:51 -0400

    microservicios tickets

---
+++ src/main/java/com/helpdesk/microservicio/tickets/MicroserviciosTicketsApplication.java
@@ -0,0 +1,13 @@
+package com.helpdesk.microservicio.tickets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviciosTicketsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviciosTicketsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/helpdesk/microservicio/tickets/MicroserviciosTicketsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.helpdesk.microservicio.tickets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviciosTicketsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
