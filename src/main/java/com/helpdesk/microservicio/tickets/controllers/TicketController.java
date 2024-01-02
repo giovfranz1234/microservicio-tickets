712c0a3d0b9587389bc07dede593b939bec17168
Giovanni Roque <giovfranz@gmail.com>
2024-01-02 13:46:12 -0400

    cambios modelo

--- src/main/java/com/helpdesk/microservicio/tickets/controllers/TicketController.java
+++ src/main/java/com/helpdesk/microservicio/tickets/controllers/TicketController.java
@@ -17,7 +17,6 @@ public class TicketController {
 
   @GetMapping
     public List <Usuario>usuariosdtick(){
-
       return ticketService.obtUsuarios();
   }
 
--- src/main/java/com/helpdesk/microservicio/tickets/models/Ticket.java
+++ src/main/java/com/helpdesk/microservicio/tickets/models/Ticket.java
@@ -10,13 +10,10 @@ public class Ticket {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String descripcion;
     private String estado;
     private String prioridad;
 
     private Usuario CreadoPor;
-
-
 
 }
