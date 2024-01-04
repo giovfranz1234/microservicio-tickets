8c8ad964a1dbb7272772c3825a93618f36348d40
Giovanni <giovfranz@gmail.com>
2024-01-04 17:45:11 -0400

    tickets

--- src/main/java/com/helpdesk/microservicio/tickets/models/Ticket.java
+++ src/main/java/com/helpdesk/microservicio/tickets/models/Ticket.java
@@ -3,17 +3,31 @@ package com.helpdesk.microservicio.tickets.models;
 import jakarta.persistence.*;
 import lombok.Data;
 
+import java.util.Date;
+
 @Data
 @Entity
 @Table(name = "tickets")
 public class Ticket {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
     private Long id;
+    @Column(name="descripcion")
     private String descripcion;
+    @Column(name="estado")
     private String estado;
+    @Column(name="prioridad")
     private String prioridad;
+    @Column(name="idUsuario")
+    private Long CreadoPor;
 
-    private Usuario CreadoPor;
-
+    @Column(name="fechaReg")
+    private Date fechaRegistro;
+    @Column(name="fechaInicio")
+    private Date fechaInicio;
+    @Column(name="fechaFin")
+    private Date fechaFin;
+    @Column(name="idHistorial")
+    private Long Historial;
 }
